cc45d6406942da014abd21fac3f74090edad7abc
Rizwan <wwwhumandotcom@gmail.com>
2021-01-11 12:26:33 +0500

    Change the fetch URL

--- public/js/main.js
+++ public/js/main.js
@@ -6,9 +6,8 @@ const weatherForm = document.querySelector('#weatherForm')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
-    let url = 'http://api.weatherstack.com/current?access_key=4798be2d59c858d5424180038d377da7&query=' + 'dallas'
+    let url = 'http://api.weatherstack.com/current?access_key=4798be2d59c858d5424180038d377da7&query=' + 'New York'
     fetch(url).then((response)=>{
-        console.log(response)
         response.json().then((data)=>{
             console.log(data)
         })
